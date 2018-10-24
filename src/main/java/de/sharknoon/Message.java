124debb4d6e4508c1adafedde20aa5d84a640fb0
Patrick Kempf <pa.kempf@web.de>
2018-10-24 11:43:08 +0200

    First commit

---
+++ src/main/java/de/sharknoon/Message.java
@@ -0,0 +1,17 @@
+package de.sharknoon;
+
+public class Message {
+    private String from;
+    private String to;
+    private String content;
+
+    public void setFrom(String username) {
+
+    }
+
+    public void setContent(String s) {
+
+    }
+
+    //standard constructors, getters, setters
+}
