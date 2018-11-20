c3ddc37384ef04754f5d27255f4044c2f750e249
Josua Frank <jo391fra@htwg-konstanz.de>
2018-11-20 15:14:03 +0100

    - removed the nanos
    Signed-off-by: Josua Frank <jo391fra@htwg-konstanz.de>

--- src/test/java/de/sharknoon/slash/networking/endpoints/login/LoginEndpointTest.java
+++ src/test/java/de/sharknoon/slash/networking/endpoints/login/LoginEndpointTest.java
@@ -4,14 +4,18 @@ import de.sharknoon.slash.database.DB;
 import de.sharknoon.slash.database.models.User;
 import de.sharknoon.slash.networking.endpoints.TestSession;
 import org.bson.types.ObjectId;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 
 import javax.websocket.Session;
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.HashSet;
+import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class LoginEndpointTest {
     
@@ -24,7 +28,7 @@ class LoginEndpointTest {
         user.username = UUID.randomUUID().toString().substring(0, 15);
         user.sessionIDs = new HashSet<>();
         user.salt = BCrypt.gensalt();
-        user.registrationDate = LocalDateTime.now();
+        user.registrationDate = LocalDateTime.now().withNano(0);
         user.password = BCrypt.hashpw("123456", user.salt);
         user.email = user.username + "@web.de";
         
