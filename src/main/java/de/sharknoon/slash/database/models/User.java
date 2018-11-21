53b36db80d40ce7392161192f49ac7f59c0e48df
etgramli <etgramlich@gmx.de>
2018-11-21 15:28:08 +0100

    Added default values for User class to prevent NPE
    Signed-off-by: etgramli <etgramlich@gmx.de>

--- src/main/java/de/sharknoon/slash/database/models/User.java
+++ src/main/java/de/sharknoon/slash/database/models/User.java
@@ -1,6 +1,7 @@
 package de.sharknoon.slash.database.models;
 
 import com.google.gson.annotations.Expose;
+import org.apache.commons.lang3.StringUtils;
 import org.bson.codecs.pojo.annotations.BsonId;
 import org.bson.types.ObjectId;
 
@@ -13,12 +14,12 @@ public class User {
     @Expose
     public ObjectId id;
     @Expose
-    public String username;
-    public String email;
-    public String password;
-    public String salt;
-    public LocalDateTime registrationDate;
-    public Set<String> sessionIDs;
-    public Set<String> deviceIDs;
+    public String username = StringUtils.EMPTY;
+    public String email = StringUtils.EMPTY;
+    public String password = StringUtils.EMPTY;
+    public String salt = StringUtils.EMPTY;
+    public LocalDateTime registrationDate = LocalDateTime.now();
+    public Set<String> sessionIDs = Set.of();
+    public Set<String> deviceIDs = Set.of();
     
 }
