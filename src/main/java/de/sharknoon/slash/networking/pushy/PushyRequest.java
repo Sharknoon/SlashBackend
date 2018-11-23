76780b173557ca4965bf96fd1d4d2f024b635c06
Josua Frank <frank.josua@gmail.com>
2018-11-23 16:57:16 +0100

    - Bugfix for Push notifications
    Signed-off-by: Josua Frank <frank.josua@gmail.com>

--- src/main/java/de/sharknoon/slash/networking/pushy/PushyRequest.java
+++ src/main/java/de/sharknoon/slash/networking/pushy/PushyRequest.java
@@ -1,7 +1,11 @@
 package de.sharknoon.slash.networking.pushy;
 
+import com.google.gson.annotations.Expose;
+
 class PushyPushRequest {
+    @Expose
     private final Object to;
+    @Expose
     private final Object data;
     
     PushyPushRequest(Object data, Object to) {
