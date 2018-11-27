9bbadffa77a470e2089acddca21888532f6c6840
Josua Frank <jo391fra@htwg-konstanz.de>
2018-11-27 08:43:46 +0100

    - fix for the tests
    Signed-off-by: Josua Frank <jo391fra@htwg-konstanz.de>

--- src/main/java/de/sharknoon/slash/database/models/Message.java
+++ src/main/java/de/sharknoon/slash/database/models/Message.java
@@ -64,31 +64,4 @@ public class Message {
         return emotion;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Message message = (Message) o;
-
-        if (type != message.type) return false;
-        if (!subject.equals(message.subject)) return false;
-        if (!content.equals(message.content)) return false;
-        if (!creationDate.equals(message.creationDate)) return false;
-        if (!sender.equals(message.sender)) return false;
-        if (emotion != message.emotion) return false;
-        return imageUrl.equals(message.imageUrl);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = type.hashCode();
-        result = 31 * result + subject.hashCode();
-        result = 31 * result + content.hashCode();
-        result = 31 * result + creationDate.hashCode();
-        result = 31 * result + sender.hashCode();
-        result = 31 * result + emotion.hashCode();
-        result = 31 * result + imageUrl.hashCode();
-        return result;
-    }
 }
